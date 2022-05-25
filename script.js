51ffeebdd77189788cc0a7f2d8f03a6994446bbd
ProteinK <konprotein@gmail.com>
2022-05-25 18:44:35 +0200

    Add form

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ const exampleBook = new Book('Hogfather', 'Terry Pratchett', 350, true);
 const exampleBook2 = new Book('The Left Hand of Darkness', 'Ursula K. Le Guin', 300, false);
 let myLibrary = [exampleBook, exampleBook2];
 const container = document.querySelector('.container');
+const form = document.querySelector('form');
+
+const newButton = document.querySelector('#newbook');
+newButton.addEventListener('click', function (e) {
+  form.classList.toggle('hidden');
+});
+
+form.addEventListener('submit', function (e) {
+  let name = this.querySelector('#name').value;
+  let author = this.querySelector('#author').value;
+  let pages = this.querySelector('#pages').value;
+  let read = this.querySelector('#read').checked;
+
+  addBookToLibrary(name, author, pages, read);
+
+  e.preventDefault();
+});
 
 function Book(name, author, pages, read) {
   this.name = name;
